faea11bdaa7f6724c1c705b97b904d6ac3556507
borisgostev <borisgostev29@gmail.com>
2024-10-20 15:13:42 +0300

    main.py

--- Backend/Data_motivation.py
+++ Backend/Data_motivation.py
@@ -1,6 +1,6 @@
 import psycopg2
 
-def connection_database_users():
+def connection_database_motivation(): #worked
     try:
         # Замените эти параметры на ваши данные подключения
         connection = psycopg2.connect(
@@ -15,39 +15,43 @@ def connection_database_users():
         return connection
     except Exception as e:
         print("Ошибка при подключении к базе данных:", e)
+        return False
 
+def add_information_motivation(id, sales_volume, closed_deals, retention_level, start_date, end_date): #worked
+    connection = connection_database_motivation()
+    try:
+        # Создаем курсор
+        cursor = connection.cursor()
+        # SQL-запрос для вставки данных
+        insert_query = """
+        INSERT INTO motivation_criteria (id, sales_volume, closed_deals, retention_level, start_date, end_date)
+        VALUES (%s, %s, %s, %s, %s, %s);
+        """
+        # Выполняем запрос с передачей параметров
+        cursor.execute(insert_query, (id, sales_volume, closed_deals, retention_level, start_date, end_date,))
+        # Зафиксировать изменения в базе данных
+        connection.commit()
+        # Закрываем курсор
+        cursor.close()
+        print("Запись успешно добавлена.")
+        return True
+    except Exception as e:
+        print(f"Ошибка при выполнении запроса: {e}")
+        connection.rollback()  # Откат изменений в случае ошибки
+        return False
 
-def get_information_users(connection, id):
+def get_information_motivation(id): #worked
+    connection = connection_database_motivation()
     try:
         cursor = connection.cursor()
-        cursor.execute("SELECT * FROM motivation_criteria WHERE id = %s", (id))
+        cursor.execute("SELECT * FROM motivation_criteria WHERE id = %s", (id,))
         user = cursor.fetchall()
         cursor.close()
         return user
     except Exception as e:
         print(f"Ошибка при выполнении запроса: {e}")
+        return False
 
-
-def add_information_users(connection, id, sales_volume, closed_deals, retention_level, start_date, end_date):
-        try:
-            # Создаем курсор
-            cursor = connection.cursor()
-            # SQL-запрос для вставки данных
-            insert_query = """
-            INSERT INTO motivation_criteria (id, sales_volume, closed_deals, retention_level, start_date, end_date)
-            VALUES (%s, %s, %s, %s, %s, %s);
-            """
-            # Выполняем запрос с передачей параметров
-            cursor.execute(insert_query, (id, sales_volume, closed_deals, retention_level, start_date, end_date))
-            # Зафиксировать изменения в базе данных
-            connection.commit()
-            # Закрываем курсор
-            cursor.close()
-            print("Запись успешно добавлена.")
-        except Exception as e:
-            print(f"Ошибка при выполнении запроса: {e}")
-            connection.rollback()  # Откат изменений в случае ошибки
-
-
-def show_results_users(user):
-    print(f"ID: {user[0]}, Sales_volume: {user[1]}, Closed_deals: {user[2]}, Retention_level: {user[3]}, Start_date: {user[4]}, End_date: {user[5]}")
+def show_results_motivation(users): #worked
+    for user in users:
+        print(f"ID: {user[0]}, Sales_volume: {user[1]}, Closed_deals: {user[2]}, Retention_level: {user[3]}, Start_date: {user[4]}, End_date: {user[5]}")
--- Backend/Data_sales.py
+++ Backend/Data_sales.py
@@ -1,6 +1,8 @@
 import psycopg2
 
-def connection_database_sales():
+
+
+def connection_database_sales(): #worked
     try:
         # Замените эти параметры на ваши данные подключения
         connection = psycopg2.connect(
@@ -11,44 +13,49 @@ def connection_database_sales():
             port="5432"
         )
 
-        print("Соединение успешно установлено")
+        #print("Соединение успешно установлено")
         return connection
     except Exception as e:
-        print("Ошибка при подключении к базе данных:", e)
+        #print("Ошибка при подключении к базе данных:", e)
+        return False
 
 
-def get_information_sales(connection, id, date):
+def get_information_sales(id, date): #worked
+    connection = connection_database_sales()
     try:
         cursor = connection.cursor()
-        cursor.execute("SELECT * FROM sales WHERE id = %s, date = %s", (id, date))
+        cursor.execute("SELECT * FROM sales WHERE id = %s AND date = %s;", (id, date,))
         sales = cursor.fetchall()
         cursor.close()
         return sales
     except Exception as e:
-        print(f"Ошибка при выполнении запроса: {e}")
+        #print(f"Ошибка при выполнении запроса: {e}")
+        return False
 
 
-def add_information_sales(connection, id, amount, date, id_sale, status):
-        try:
-            # Создаем курсор
-            cursor = connection.cursor()
-            # SQL-запрос для вставки данных
-            insert_query = """
-            INSERT INTO sales (id, amount, date, id_sale, status)
-            VALUES (%s, %s, %s, %s, %s);
-            """
-            # Выполняем запрос с передачей параметров
-            cursor.execute(insert_query, (id, amount, date, id_sale, status))
-            # Зафиксировать изменения в базе данных
-            connection.commit()
-            # Закрываем курсор
-            cursor.close()
-            print("Запись успешно добавлена.")
-        except Exception as e:
-            print(f"Ошибка при выполнении запроса: {e}")
-            connection.rollback()  # Откат изменений в случае ошибки
+def add_information_sales(id, amount, date, id_product, status): #worked
+    connection = connection_database_sales()
+    try:
+        # Создаем курсор
+        cursor = connection.cursor()
+        # SQL-запрос для вставки данных
+        insert_query = """
+        INSERT INTO sales (id, amount, date, id_product, status)
+        VALUES (%s, %s, %s, %s, %s);
+        """
+        # Выполняем запрос с передачей параметров
+        cursor.execute(insert_query, (id, amount, date, id_product, status))
+        # Зафиксировать изменения в базе данных
+        connection.commit()
+        # Закрываем курсор
+        cursor.close()
+        #print("Запись успешно добавлена.")
+    except Exception as e:
+        #print(f"Ошибка при выполнении запроса: {e}")
+        connection.rollback()  # Откат изменений в случае ошибки
+        return False
 
 
-def show_results_sales(sales):
+def show_results_sales(sales): #worked
     for sale in sales:
-        print(f"ID: {sale[0]}, Amount: {sale[1]}, Date: {sale[2]}, Id_sale: {sale[3]}, Status: {sale[4]}")
+        print(f"ID: {sale[0]}, Amount: {sale[1]}, Date: {sale[2]}, Id_product: {sale[3]}, Status: {sale[4]}")
