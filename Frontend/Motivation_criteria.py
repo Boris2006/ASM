1954343a8dfcc63db120f711db7324adbf0dab68
Boris <92259287+Boris2006@users.noreply.github.com>
2024-10-22 16:42:42 +0300

    Motivation_criteria.py

--- Frontend/Motivation_criteria.py
+++ Frontend/Motivation_criteria.py
@@ -1,3 +1,6 @@
+from Backend.Data_motivation import add_information_motivation
+
+
 class MotivationCriteria:
     def __init__(self, sales_volume, closed_deals, retention_level, start_date, end_date):
         self.sales_volume = sales_volume
@@ -12,3 +15,12 @@ class MotivationCriteria:
         self.retention_level = retention_level
         self.start_date = start_date
         self.end_date = end_date
+    
+    def set_criteria(sales_volume, closed_deals, retention_level, start_date, end_date):
+        try:
+            add_information_motivation(sales_volume, closed_deals, retention_level, start_date, end_date)
+            print("Критерии мотивации успешно установлены")
+            return True
+        except Exception as e:
+            print(f"Ошибка при установке критериев мотивации: {e}")
+            return False
